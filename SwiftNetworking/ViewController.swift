82a93340d1b6db46f02c959e6681eef1346113de
Martino Christanto Khuangga <13514084@std.stei.itb.ac.id>
2018-11-14 08:24:46 +0700

    Initial Commit

---
+++ SwiftNetworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftNetworking
+//
+//  Created by Nakama on 14/11/18.
+//  Copyright © 2018 Tokopedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
